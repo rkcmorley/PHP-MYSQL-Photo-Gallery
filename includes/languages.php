50232535778817afda00c014735a4c32af28bf62
Richard Morley <richard@Richards-MacBook-Pro.local>
2020-04-14 23:45:07 +0100

    Fixed language errors

--- includes/languages.php
+++ includes/languages.php
@@ -1,13 +1,21 @@
-git<?php
+<?php
+
+if ($_GET['page'] == 'home' OR isset($_GET['image'])) {
+    $page_title = $lang['home_title'];
+    $page_heading = $lang['home_heading'];
+}
+
+if($_GET['page'] == 'uploads'){
+    $page_title = $lang['upload_title'];
+    $page_heading = $lang['upload_heading'];
+}
+
 $home_link = $lang['home_link'];
 $upload_link = $lang['upload_link'];
-$page_title = $lang['upload_title'];
-$page_heading = $lang['upload_heading'];
 
 $header = file_get_contents('templates/header.html');
 $header = str_replace('[+home+]', $home_link, $header);
 $header = str_replace('[+upload+]', $upload_link, $header);
 $header = str_replace('[+page_title+]', $page_title, $header);
 $header = str_replace('[+page_heading+]', $page_heading, $header);
-
 echo $header;
